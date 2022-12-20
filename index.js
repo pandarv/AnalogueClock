be749c225b5a7f71aa9d5d94d75abb5e22eebebe
Arv <pandit.arv@gmail.com>
2022-12-20 05:07:58 -0700

    JS for analogue watch

---
+++ index.js
@@ -0,0 +1,19 @@
+const hNeedle = document.querySelector('.hour-needle')
+const mNeedle = document.querySelector('.minute-needle')
+const sNeedle = document.querySelector('.second-needle')
+
+setInterval(() => {
+  const currentDate = new Date()
+  const hour = currentDate.getHours()
+  const minute = currentDate.getMinutes()
+  const second = currentDate.getSeconds()
+  
+  hNeedle.style.transform = `rotate(${(30*hour)+(minute/2)+(second/120)}deg)`
+  mNeedle.style.transform = `rotate(${(6*minute)+(second/10)}deg)`
+  sNeedle.style.transform = `rotate(${6*second}deg)`
+}, 1000)
+/*
+Hour Needle = (30H + M/2 + S/120)deg
+Minute Needle = (6M)deg
+Second Needle = (6S)deg
+*/
