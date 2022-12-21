7930f78801b0d7512d5d23512bb4402b28b14d00
Arv <pandit.arv@gmail.com>
2022-12-20 22:05:16 -0700

    created new clock with better organized css and html code

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const hNeedle = document.querySelector('.hour-needle')
 const mNeedle = document.querySelector('.minute-needle')
 const sNeedle = document.querySelector('.second-needle')
+const dialHr = document.querySelectorAll('.dial-hr')
+
+// console.log(dialHr)
+dialHr.forEach((hr, index) => hr.style.transform = `rotate(${30 * index}deg)` )
 
 setInterval(() => {
   const currentDate = new Date()
   const hour = currentDate.getHours()
   const minute = currentDate.getMinutes()
   const second = currentDate.getSeconds()
-  
+
   hNeedle.style.transform = `rotate(${(30*hour)+(minute/2)+(second/120)}deg)`
   mNeedle.style.transform = `rotate(${(6*minute)+(second/10)}deg)`
   sNeedle.style.transform = `rotate(${6*second}deg)`
